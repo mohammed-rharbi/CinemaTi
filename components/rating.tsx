4fef266baebfbfb29df1e02e9c49e2c5bf14b423
mohammed-rharbi <rharbi383@gmail.com>
2025-02-06 18:26:54 +0100

    feat(components): add rating component

---
+++ components/rating.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput, Dimensions, Animated } from "react-native"
+import { LinearGradient } from "expo-linear-gradient"
+import { Ionicons } from "@expo/vector-icons"
+
+const Rating = () => {
+
+
+  return (
+
+    <View style={styles.container}>
+    
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+  },
+  listContent: {
+    padding: 16,
+    paddingTop: 80,
+  },
+})
+
+export default Rating
+
